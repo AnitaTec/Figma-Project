05b705811cd3ea4e48d4157dded30ba21cad1ce9
AnitaTec <anitateclava@gmail.com>
2025-07-07 10:09:28 +0200

    Sett up

---
+++ src/pages/home/index.jsx
@@ -0,0 +1,13 @@
+import unionIcon from "../../assets/unionIcon";
+import Header from "./components/Header";
+import Main from 
+function Home() {
+  return;
+  <>
+    <Header />
+    <Main />
+    <Footer />
+  </>;
+}
+
+export default Home;
